24d3d6500f2eb0843f7a954e7433a7dc7360cc3c
Raghu Gannaram <raghuvarangannaram@gmail.com>
2022-06-12 18:21:16 +0530

    Updated the UI

--- routes/routes.js
+++ routes/routes.js
@@ -3,74 +3,40 @@ const url = require("url");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const db_model = require("../controller/db_module");
-const readCookie =require("../utilities/cookieParser")
-
+const readCookie = require("../utilities/cookieParser");
+const {styleSheets, publicImages} = require("./commonHandler");
 
 dotenv.config();
 
-const connection = mongoose.connect(process.env.MongoDB_URL,{
-                                useNewUrlParser: true,
-                                useUnifiedTopology: true,
-                                useCreateIndex:true,  })
-                            .then(()=> console.log("connected to MongoDB Database"))
-                            .catch(err => console.log(err) )
+const connection = mongoose
+  .connect(process.env.MongoDB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("connected to MongoDB Database"))
+  .catch((err) => console.log(err));
 
 const routeHandlers = (req, res) => {
-  
-  const path = url.parse(req.url).pathname;
+  const baseURL =  req.protocol + '://' + req.headers.host + '/';
+  const reqURL = new URL(req.url,baseURL);
+  // console.log(reqURL);
+
+  const path = reqURL.pathname;
   console.log("\n \t current route is " + path);
 
-  const renderHTML = (path) => {
-    fs.readFile(path, null, (err, html) => {
-      console.log("\n \t current path is " + path);
+  styleSheets(req, res, path);
+  publicImages(req, res, path);
+
+  const renderHTML = (fileLocation) => {
+    fs.readFile(fileLocation, null, (err, html) => {
+      console.log("\n \t current path is " + fileLocation);
       if (err) throw err;
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(html);
       res.end();
     });
   };
-
-  if (path == "/public/favicon.png") {
-    res.writeHead(200, { "Content-type": "image/jpg" });
-    let img = fs.readFileSync("./public/favicon.png", "binary");
-    res.write(img, "binary");
-    res.end();
-  }
-
-  if (path == "/images/wallpaper.jpg") {
-    res.writeHead(200, { "Content-type": "image/jpg" });
-    let img = fs.readFileSync("./public/wallpaper.jpg", "binary");
-    res.write(img, "binary");
-    res.end();
-  }
-
-  if (path == "/images/ladakh.jpg") {
-    res.writeHead(200, { "Content-type": "image/jpg" });
-    let img = fs.readFileSync("./public/ladakh.jpg", "binary");
-    res.write(img, "binary");
-    res.end();
-  }
-
-  if (path == "/images/daramshala.jpg") {
-    res.writeHead(200, { "Content-type": "image/jpg" });
-    let img = fs.readFileSync("./public/daramshala.jpg", "binary");
-    res.write(img, "binary");
-    res.end();
-  }
-
-  if (path == "/images/manalitoleah.jpg") {
-    res.writeHead(200, { "Content-type": "image/jpg" });
-    let img = fs.readFileSync("./public/manalitoleah.jpg", "binary");
-    res.write(img, "binary");
-    res.end();
-  }
-
-  if (path == "/style.css") {
-    res.writeHead(200, { "Content-Type": "text/css" });
-    let content = fs.readFileSync("./view/style.css", "utf8");
-    res.write(content);
-    res.end();
-  }
 
   switch (path) {
     case "/": {
@@ -99,7 +65,7 @@ const routeHandlers = (req, res) => {
         "Set-Cookie": [
           `isSignedIn= ${null}`,
           `userId=${null}`,
-          `username=${null}`
+          `username=${null}`,
         ],
       });
       res.end();
@@ -110,20 +76,19 @@ const routeHandlers = (req, res) => {
       break;
     }
     case "/postablog": {
-      if(readCookie(req.headers.cookie, "isSignedIn") == "true"){
+      if (readCookie(req.headers.cookie, "isSignedIn") == "true") {
         renderHTML("./view/postablog.html");
         break;
-      }else{
-        res.writeHead(302,{Location : "/signin"})
+      } else {
+        res.writeHead(302, { Location: "/signin" });
         res.end();
         break;
       }
     }
 
-
     //DB Interaction Involved
 
-    case "/fetchblogs":{
+    case "/fetchblogs": {
       db_model.blogposts(req, res);
       break;
     }
@@ -134,13 +99,13 @@ const routeHandlers = (req, res) => {
         signupdata += chunk;
       });
       req.on("end", () => {
-        console.log("signupdata",signupdata)
+        console.log("signupdata", signupdata);
         let searchParams = new URLSearchParams(signupdata);
-        let userobj ={
-          username : searchParams.get("username"),
-          usermail : searchParams.get("usermail"),
-          userpassword : searchParams.get("userpassword")
-        }
+        let userobj = {
+          username: searchParams.get("username"),
+          usermail: searchParams.get("usermail"),
+          userpassword: searchParams.get("userpassword"),
+        };
         db_model.signupuser(req, res, userobj);
       });
       break;
@@ -152,12 +117,12 @@ const routeHandlers = (req, res) => {
         signindata += chunk;
       });
       req.on("end", () => {
-        console.log("signin data", signindata)
+        console.log("signin data", signindata);
         let searchParams = new URLSearchParams(signindata);
-        let userobj ={
-          usermail : searchParams.get("usermail"),
-          userpassword : searchParams.get("userpassword")
-        }
+        let userobj = {
+          usermail: searchParams.get("usermail"),
+          userpassword: searchParams.get("userpassword"),
+        };
         db_model.signinuser(req, res, userobj);
       });
       break;
@@ -171,51 +136,49 @@ const routeHandlers = (req, res) => {
       req.on("end", () => {
         let searchParams = new URLSearchParams(userpostdata);
         let blogobj = {
-          title : searchParams.get("blogtitle"),
-          author : searchParams.get("blogauthor"),
-          body : searchParams.get("blogcontent")
-        }
+          title: searchParams.get("blogtitle"),
+          author: searchParams.get("blogauthor"),
+          body: searchParams.get("blogcontent"),
+        };
         db_model.postblog(req, res, blogobj);
       });
       break;
     }
-   
+
     case "/postcomment": {
-      if(readCookie(req.headers.cookie, "isSignedIn") == "true"){
+      if (readCookie(req.headers.cookie, "isSignedIn") == "true") {
         let commentdata = "";
         req.on("data", (chunk) => {
           commentdata += chunk;
         });
         req.on("end", () => {
-          console.log("the commentdata "+ commentdata)
-          console.log("the type of commentdata "+ typeof(commentdata))
+          console.log("the commentdata " + commentdata);
+          console.log("the type of commentdata " + typeof commentdata);
           db_model.postcomment(req, res, commentdata);
         });
         break;
-      }
-      else{
-        res.writeData(302, { Location: "/signin"});
-        res.end()
+      } else {
+        res.writeData(302, { Location: "/signin" });
+        res.end();
         break;
       }
     }
 
     case "/postlike": {
-      if(readCookie(req.headers.cookie, "isSignedIn") == "true"){
+      if (readCookie(req.headers.cookie, "isSignedIn") == "true") {
         let likedata = "";
         req.on("data", (chunk) => {
           likedata += chunk;
         });
         req.on("end", () => {
-          console.log("the likedata "+ likedata)
-          console.log("the type of likedata "+ typeof(likedata))
+          console.log("the likedata " + likedata);
+          console.log("the type of likedata " + typeof likedata);
           db_model.postlike(req, res, likedata);
         });
         break;
-      }
-      else{
-        res.writeData(302, { Location: "/signin"});
-        res.end()
+      } else {
+        res.writeData(302, { Location: "/signin" });
+        res.end();
         break;
       }
     }
@@ -230,10 +193,10 @@ const routeHandlers = (req, res) => {
       });
       break;
     }
-    
+
     default: {
-      console.log("\n \t print from default route" + path);
-      // if(path.includes("public") || path == "/style.css"){
+      // console.log("\n \t print from default route" + path);
+      // if(path.includes("public") || path == "/css/style.css"){
       //   break;
       // }
       // else{
